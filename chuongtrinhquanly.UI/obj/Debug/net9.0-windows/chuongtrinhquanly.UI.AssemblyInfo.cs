9e34a4d1b7c7a0c299c443b2813775c93c3f2fae
Phan Vũ Hoàng Hải <phanhaiph999@gmail.com>
2025-11-12 18:11:36 +0700

    bai bao cao

--- chuongtrinhquanly.UI/obj/Debug/net9.0-windows/chuongtrinhquanly.UI.AssemblyInfo.cs
+++ chuongtrinhquanly.UI/obj/Debug/net9.0-windows/chuongtrinhquanly.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chuongtrinhquanly.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f12cf67d9882b7d03283427849e79720c2d9631")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f9b98c0b1a382d43b6120649abc3ed3168d356")]
 [assembly: System.Reflection.AssemblyProductAttribute("chuongtrinhquanly.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chuongtrinhquanly.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
